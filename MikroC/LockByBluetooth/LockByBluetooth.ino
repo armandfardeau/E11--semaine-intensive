fd1198407eac7f9ca9648dbd087975c01dc514d9
armandfardeau <fardeauarmand@gmail.com>
2016-12-11 19:14:09 +0100

    add function convert User time from string to int / opening is working
    TODO calculate time left

--- MikroC/LockByBluetooth/LockByBluetooth.ino
+++ MikroC/LockByBluetooth/LockByBluetooth.ino
@@ -20,6 +20,8 @@ Servo myServo;
   int open = 90;
   //Position fermé = 10 degré
   int close = 10;
+  //boolean boxclosed
+  bool boxClosed;
 
 //var used in serial communication part
   char junk;
@@ -27,6 +29,93 @@ Servo myServo;
   String dateUser;
   String boxTime = "";
 
+//var used to split the user time 
+int dayUser;
+int monthUser;
+int yearUser;
+int hourUser;
+int minuteUser;  
+
+
+//function declaration
+void FunctionStringToInt(String _string){
+  int i;
+  for( i = 0; i< _string.length(); i++){
+    switch (i) {
+        case 0:
+         dayUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+        case 1:
+         dayUser += FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 2:
+         monthUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 3:
+        monthUser += FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 4:
+          yearUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 5:
+          yearUser += FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 6:
+          yearUser += FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 7:
+          yearUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 8:
+          hourUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 9:
+          hourUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 10:
+          minuteUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+          case 11:
+          minuteUser = FunctionCompare(_string[i])*pow(10,(_string.length()-i-1));
+          break;
+      } 
+    } 
+}
+
+int FunctionCompare(char _char){
+    if(_char == "0"){
+        return 0;
+    }
+    if(_char == "1"){
+      return 1;
+    }
+    if(_char == "2"){
+      return 2;  
+    }
+    if(_char == "3"){
+      return 3;  
+    }
+    if(_char == "4"){
+      return 4;  
+    }
+    if(_char == "5"){
+      return 5;  
+    }
+    if(_char == "6"){
+      return 6;  
+    }
+    if(_char == "7"){
+      return 7;  
+    }
+    if(_char == "8"){
+      return 8;  
+    }
+    if(_char == "9"){
+      return 9;  
+    }
+}
+
+unsigned long FunctionTimeLeft(actualDay,actualMonth,actualYear,actualHour,actualMinute,futureDay,futureMonth,futureYear,futureHour,futureMinute){}
 void setup() {
 //RTC module begin
   Wire.begin();
@@ -63,7 +152,11 @@ void loop()
   DateTime now = RTC.now();
   //Serial.print(now.minute());
   //Serial.println(now.second());
-  //boxTime = String(now.minute());
+  boxTime = String(now.day());
+  boxTime += String(now.month());
+  boxTime += String(now.year());
+  boxTime += String(now.hour());
+  boxTime += String(now.minute());
 
 
   if(Serial.available()){
@@ -77,21 +170,33 @@ void loop()
       
       if(inputString == "close"){   //incase of 'close' close the box
         digitalWrite(13, HIGH);
+        boxClosed = true;
          myServo.write(close);
       }
       else if(inputString == "open"){   //incase of 'open' open the box
         digitalWrite(13, HIGH);
+        boxClosed = false;
          myServo.write(open);
       }
       //if input is not empty then it must be full of something, like the numbers we need
       else{
         dateUser = inputString;
-        Serial.println(dateUser);
+        FunctionStringToInt(dateUser);
       }
       while (Serial.available() > 0)  
       { junk = Serial.read() ; }      // clear the serial buffer
       inputString = "";
     }
-  else{
+  else if (boxClosed == true){
+    if(dateUser == boxTime){
+      digitalWrite(13, HIGH);
+      boxClosed = false;
+      myServo.write(open);
+      }
     }
+  else{}
 }
+
+
+
+  
