2092044d513b0eb714038515bbb43423935218b7
armandfardeau <fardeauarmand@gmail.com>
2016-12-12 15:37:52 +0100

    Make animation for opening

--- MikroC/ourPulseLight/ourPulseLight.ino
+++ MikroC/ourPulseLight/ourPulseLight.ino
@@ -5,22 +5,27 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIN);
 const uint32_t colorMap[COLOR_COUNT] = {
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
-  strip.Color(64, 0, 0),
+  strip.Color(106, 162, 203),
+  strip.Color(94, 11, 212),
+  strip.Color(89, 178, 213),
+  strip.Color(98, 170, 208),
+  strip.Color(111, 153, 198),
+  strip.Color(123, 8, 183),
+  strip.Color(131, 111, 171),
+  strip.Color(135, 100, 164),
+  strip.Color(135, 97, 162),
+  strip.Color(133, 103, 166),
+  strip.Color(128, 120, 177),
+  strip.Color(119, 141, 191),
 };
 
-int pulse = 24;
-int brightness[3] = {0,100, 5}; // max/min brightness
+float fadeInValue = 0.5;
+float fadeOutValue = 0.5;
+int fadeMax = 75;
+int fadeMin = 10;
+int dimming = 15;
+int pulse = 3;
+int i;
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,31 +39,33 @@ void loop() {
 
 
 
+  while (i < pulse) {
+    for (float fadeValue = fadeMin ; fadeValue <= fadeMax; fadeValue += fadeInValue) {
+      // sets the value (range from 0 to 75):
+      for (int i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, colorMap[i]);
 
-
-  for (int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, colorMap[i]);
+      }
+      strip.setBrightness(fadeValue);
+      strip.show(); // Initialize all pixels to 'off'
+      // wait for 30 milliseconds to see the dimming effect
+      delay(dimming);
+    }
+    // fade out from max to min in increments of 5 points:
+    for (float fadeValue = fadeMax ; fadeValue >= fadeMin; fadeValue -= fadeOutValue) {
+      for (int i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, colorMap[i]);
+      }
+      // sets the value (range from 0 to 255):
+      strip.setBrightness(fadeValue);
+      strip.show(); // Initialize all pixels to 'off'
+      // wait for 30 milliseconds to see the dimming effect
+      delay(dimming);
+    }
+    i++;
   }
-  for (int i = 0; i <= pulse; i++ && pulse != 0) {
-    if (pulse % 2 == 0) {
-      strip.setPixelColor(i, colorMap[i]);
-      strip.setBrightness(brightness[1]);
-      strip.show();
-      Serial.println("modulo 2");
-    }
-    else if (pulse != 0) {
-      strip.setBrightness(brightness[2]);
-      strip.show();
-      Serial.println("!modulo 2");
-    }
-    else {
-      strip.setBrightness(brightness[0]);
-      strip.show();
-    }
-  }
-  delay(2000);
-  Serial.println(pulse);
-  pulse--;
+  strip.setBrightness(0);
+  strip.show();
 }
 
 
